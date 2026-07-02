aa61829058ed7876d68f597d12215a6eb783639a
MauricioSuilice19 <lucasuilice@hotmail.com.ar>
2026-07-02 05:16:47 -0300

    Agregar componentes CalendarioTurnos y CatalogoServicios con sus respectivos archivos HTML, CSS y pruebas

---
+++ src/app/components/calendario-turnos/calendario-turnos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendario-turnos',
+  imports: [],
+  templateUrl: './calendario-turnos.html',
+  styleUrl: './calendario-turnos.css',
+})
+export class CalendarioTurnos {}
