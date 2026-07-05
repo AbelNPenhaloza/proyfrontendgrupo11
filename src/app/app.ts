cb6bedcb8867a877de2e19e9fdfaa1d89103c93a
JLM547 <jeremiasmorales547@gmail.com>
2026-07-05 16:38:23 -0300

    corregir app.ts y restaurar cambios del barbero

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './shared/navbar-admin/navbar';
 import { NavbarBarberoComponent } from './shared/navbar-barbero/navbar-barbero';
 import { AuthService } from './services/auth/auth.service';
 import { Home } from './components/home/home';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, NavbarComponent, Home, NavbarBarberoComponent],
@@ -11,15 +12,12 @@ import { Home } from './components/home/home';
   styleUrl: './app.css'
 })
 export class App {
-
-constructor(private auth: AuthService) {
-  this.auth.logout();
-  // Si no hay token, limpiar el rol
-  if (!this.authService.isAuthenticated()) {
-    localStorage.removeItem('role');
-  }
-}
-
   protected readonly authService = inject(AuthService);
   protected readonly title = signal('frontend');
+
+  constructor() {
+    if (!this.authService.isAuthenticated()) {
+      localStorage.removeItem('role');
+    }
+  }
 }
